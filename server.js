ca7406b71a1e74eae4e945310185d832b9f20e2a
nicolas <nhomble@terpmail.umd.edu>
2017-09-13 00:51:19 -0400

    cors

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  } else {
+    return next();
+  }
+});
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
