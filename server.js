196cf9ee097c08eaba9b9bc812aa4f2ffbdb5c3c
nicolas <nhomble@terpmail.umd.edu>
2017-09-14 00:15:05 -0400

    don't need cors here, needed it in the vertx api

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 // server.js
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
 
-app.use(cors());
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
