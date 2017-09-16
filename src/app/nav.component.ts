f5033df7c859daa960eb5c3c46462a6eb2dd99f0
nicolas <nhomble@terpmail.umd.edu>
2017-09-16 18:22:20 -0400

    display languages supported at top bar

--- src/app/nav.component.ts
+++ src/app/nav.component.ts
@@ -1,8 +1,7 @@
 /**
  * Created by nicolas on 9/14/2017.
  */
-import {Component} from "@angular/core";
-import {AppComponent} from "./app.component";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -12,4 +11,29 @@ import {AppComponent} from "./app.component";
   ]
 })
 export class NavComponent {
+
+  questionMap = new Map<string, string[]>();
+  @Input() languages: string[] = [];
+  @Input() set questions(qList: string[]) {
+    let tmpMap = new Map<string, string[]>();
+    // should fold
+    for (let q of qList) {
+      let parts = q.split("/");
+      if (tmpMap.has(parts[0])) {
+        let newArr = tmpMap.get(parts[0]);
+        newArr.push(parts[1]);
+        tmpMap.set(parts[0], newArr);
+      }
+      else
+        tmpMap.set(parts[0], [parts[1]]);
+    }
+    this.questionMap = tmpMap;
+  }
+
+  @Output() onQuestionSelected = new EventEmitter<string>();
+
+  pickQuestion(q: string) {
+    this.onQuestionSelected.emit(q);
+    console.log("chose question: " + q);
+  }
 }
